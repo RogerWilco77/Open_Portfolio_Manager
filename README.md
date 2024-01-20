78d7a12e2c91f6b05609e2d841b4761ab784ec15
Axel <Axel@wilbertz.net>
2024-01-20 16:38:18 +0100

    added a README.md

---
+++ README.md
@@ -0,0 +1,4 @@
+# Open Portfolio Manager
+
+This is a hobby project to select potential stock for a portfolio and backtest investment strategies.
+It is a pure hobby project and does not represent any investment advice.
---
+++ main.py
@@ -0,0 +1,10 @@
+from PySide6.QtWidgets import QApplication, QWidget, QMainWindow
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+   app = QApplication()
+   window = QMainWindow()
+   window.show()
+   window.setWindowTitle('Open Portfolio Manager')
+
+   app.exec()
